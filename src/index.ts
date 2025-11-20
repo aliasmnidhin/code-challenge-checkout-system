f9ab6073230263364f5fa081e250cb373f126827
Alias M Nidhin Peter <aliasmnidhinpeter1992@gmail.com>
2025-11-20 15:50:30 +0530

    chore: add index.ts for manual checkout testing

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Checkout } from "./services/checkout";
+import { bulkDiscount, multiBuyOffer } from "./rules/pricingRules";
+import { PRODUCTS } from "./data/products";
+
+const pricingRules = [
+  multiBuyOffer("atv", 3, 2), // Apple TV: 3 for 2 deal
+  bulkDiscount("ipd", 4, 499.99), // Brand new Super iPad bulk discount > 4 items
+];
+
+const co = new Checkout(pricingRules);
+
+// Case 1: SKUs Scanned: atv, atv, atv, vga. Total expected: 249.00
+// co.scan(PRODUCTS.atv);
+// co.scan(PRODUCTS.atv);
+// co.scan(PRODUCTS.atv);
+// co.scan(PRODUCTS.vga);
+
+// Case 2: SKUs Scanned: atv, ipd, ipd, atv, ipd, ipd, ipd. Total expected: 2718.95
+co.scan(PRODUCTS.atv);
+co.scan(PRODUCTS.ipd);
+co.scan(PRODUCTS.ipd);
+co.scan(PRODUCTS.atv);
+co.scan(PRODUCTS.ipd);
+co.scan(PRODUCTS.ipd);
+co.scan(PRODUCTS.ipd);
+
+const total = co.total();
+
+console.log("Total:", total);
